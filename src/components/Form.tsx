c76d805d3da18872e87108d64d813fbb5004f152
Ayush Vijayant <ayush.vijayant@gmail.com>
2022-04-25 23:05:57 +0530

    refactor

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,7 @@ import { Link, navigate } from "raviger";
 import React, { useState, useEffect, useRef } from "react";
 import { LabellebInput } from "../LabelledInput";
 import Dropdown from "./Dropdown";
+import Radio from "./Radio";
 
 export interface formData {
     id: number,
@@ -27,7 +28,7 @@ export type DropdownField = {
     value: string[]
 }
 
-type RadioField = {
+export type RadioField = {
     kind: "radio",
     id: number,
     label: string,
@@ -79,12 +80,6 @@ const saveFormData = (currentState: formData) => {
     saveLocalForms(updatedLocalForms);
 };
 
-const fetchForms = (setFormsCB: (value: formData[]) => void) => {
-    fetch("https://tsapi.coronasafe.live/api/mock_test/").
-        then((response) => response.json()).
-        then((data) => setFormsCB(data));
-}
-
 export function Form(props: { formId: number }) {
     const [state, setState] = useState(() => initialState(props.formId));
     const [newField, setNewField] = useState("");
@@ -101,10 +96,6 @@ export function Form(props: { formId: number }) {
             console.log("component UNMOUNTED");
             document.title = "React App";
         }
-    }, [])
-
-    useEffect(() => {
-        // fetchForms();
     }, [])
 
     useEffect(() => {
@@ -198,56 +189,8 @@ export function Form(props: { formId: number }) {
         setState(state1)
     }
 
-    const setRadioOptionLabel = (option: string, id: number, index: number) => {
-        setState({
-            ...state,
-            formFields: state.formFields.map(field => {
-                if (field.id === id && field.kind === "radio") {
-                    field.options[index] = option
-                    return field
-                }
-                return field
-            })
-        })
-    }
 
-    const initialRadioValue = (id: number) => {
-        const { value } = state.formFields.filter(field => field.id === id && field.kind === "radio")[0];
-        return typeof value === "string" ? value : ''
-    }
-    const [radioOptionValue, setradioOptionValue] = useState(() => initialRadioValue(6))
 
-    const saveRadioState = (checkedOption: string, id: number, options: string[]) => {
-        radioOptionValue === checkedOption ? setradioOptionValue('') : setradioOptionValue(checkedOption)
-        setState({
-            ...state,
-            formFields: state.formFields.map(field => {
-                if (field.id === id)
-                    return {
-                        ...field,
-                        kind: "radio",
-                        options: options,
-                        value: checkedOption
-                    }
-                return field
-            })
-        })
-    }
-
-    const addNewRadio = () => {
-        setState({
-            ...state,
-            formFields: [...state.formFields,
-            { kind: "radio", id: Number(new Date()), label: "New Radio", options: ["React", "Angular", "Vue"], value: '' }
-            ]
-        })
-    }
-    const removeRadio = (id: number) => {
-        setState({
-            ...state,
-            formFields: state.formFields.filter(field => field.id !== id)
-        })
-    }
     return (
         <div className='flex flex-col gap-4 p-4 divide-y '>
             <input type="text" ref={titleRef} value={state.title} className="border-2 border-gray-200 rounded-lg pd-2 m-2 flex-1"
@@ -278,29 +221,13 @@ export function Form(props: { formId: number }) {
                             />
 
                         case "radio":
-                            return (<div className="" key={field.id}>
-                                <label>
-                                    <input type="text" value={field.label} onChange={e => setFieldLabel(e.target.value, field.id)} className="border-2 border-gray-200 rounded-lg m-2" />
-                                </label>
-                                {
-                                    field.options.map((option) => {
-                                        return (<div >
-                                            <input type="radio"
-                                                onChange={() => saveRadioState(option, field.id, field.options)}
-                                                checked={radioOptionValue === option ? true : false} className="m-2" value={option} />
-                                            <label >
-                                                <input type="text" className="border-2 border-gray-200 rounded-lg m-2"
-                                                    value={option}
-                                                    onChange={e => setRadioOptionLabel(e.target.value, field.id, field.options.indexOf(option))} />
-                                            </label>
-                                        </div>
-                                        )
-                                    })
-                                }
-                                {/* <button onClick={_ => addNewRadio()} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mx-2 rounded-lg'>Add</button> */}
-                                <button onClick={_ => removeRadio(field.id)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mx-2 rounded-lg'>Remove</button>
-
-                            </div>)
+                            return <Radio
+                                key={field.id}
+                                field={field}
+                                setFieldLabelCB={setFieldLabel}
+                                setStatepropCB={setStateprop}
+                                state={state}
+                            />
                         default:
                             break;
                     }
--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,8 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { formData, RadioField } from "./Form";
 
-export default function Radio() {
+export default function Radio(props: {
+    field: RadioField,
+    setStatepropCB: (state1: formData) => void,
+    state: formData,
+    setFieldLabelCB: (label: string, fieldId: number) => void
+}) {
+
+    const initialRadioValue = (id: number) => {
+        const { value } = props.state.formFields.filter(field => field.id === id && field.kind === "radio")[0];
+        return typeof value === "string" ? value : ''
+    }
+    const [radioOptionValue, setradioOptionValue] = useState(() => initialRadioValue(props.field.id))
+
+    const saveRadioState = (checkedOption: string, id: number, options: string[]) => {
+        radioOptionValue === checkedOption ? setradioOptionValue('') : setradioOptionValue(checkedOption)
+        props.setStatepropCB({
+            ...props.state,
+            formFields: props.state.formFields.map(field => {
+                if (field.id === id)
+                    return {
+                        ...field,
+                        kind: "radio",
+                        options: options,
+                        value: checkedOption
+                    }
+                return field
+            })
+        })
+    }
+    const setRadioOptionLabel = (option: string, id: number, index: number) => {
+        props.setStatepropCB({
+            ...props.state,
+            formFields: props.state.formFields.map(field => {
+                if (field.id === id && field.kind === "radio") {
+                    field.options[index] = option
+                    return field
+                }
+                return field
+            })
+        })
+    }
+    const removeRadio = (id: number) => {
+        props.setStatepropCB({
+            ...props.state,
+            formFields: props.state.formFields.filter(field => field.id !== id)
+        })
+    }
 
     return (
-        <div className=""></div>
+        <div className="" key={props.field.id}>
+            <label>
+                <input type="text" value={props.field.label} onChange={e => props.setFieldLabelCB(e.target.value, props.field.id)} className="border-2 border-gray-200 rounded-lg m-2" />
+            </label>
+            {
+                props.field.options.map((option) => {
+                    return (<div >
+                        <input type="radio"
+                            onChange={() => saveRadioState(option, props.field.id, props.field.options)}
+                            checked={radioOptionValue === option ? true : false} className="m-2" value={option} />
+                        <label >
+                            <input type="text" className="border-2 border-gray-200 rounded-lg m-2"
+                                value={option}
+                                onChange={e => setRadioOptionLabel(e.target.value, props.field.id, props.field.options.indexOf(option))} />
+                        </label>
+                    </div>
+                    )
+                })
+            }
+            {/* <button onClick={_ => addNewRadio()} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mx-2 rounded-lg'>Add</button> */}
+            <button onClick={_ => removeRadio(props.field.id)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 mx-2 rounded-lg'>Remove</button>
+
+        </div>
     )
 }
